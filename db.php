bbd0044fddaad6879ae73a18ecb04688c2f14e12
Mark Hobson <mark.hobson@blackpepper.co.uk>
2017-03-17 09:51:31 +0000

    Removed database config setting documentation in favour of Craft docs

--- db.php
+++ db.php
@@ -9,19 +9,14 @@
 
 return array(
 
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
 	'server' => getenv('CRAFT_DATABASE_HOST'),
 
-	// The name of the database to select.
 	'database' => getenv('CRAFT_DATABASE_NAME'),
 
-	// The database username to connect with.
 	'user' => getenv('CRAFT_DATABASE_USER'),
 
-	// The database password to connect with.
 	'password' => getenv('CRAFT_DATABASE_PASSWORD'),
 
-	// The prefix to use when naming tables. This can be no more than 5 characters.
 	'tablePrefix' => 'craft',
 
 );
